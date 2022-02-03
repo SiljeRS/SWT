436bce2c6c2e4dfd472351c645ebbe7213a59435
SiljeRS <68823039+SiljeRS@users.noreply.github.com>
2022-02-03 13:36:00 +0100

    Add project files.

---
+++ Calculator.Test.Unit/UnitTest1.cs
@@ -0,0 +1,77 @@
+using NUnit.Framework;
+using SimpleCalculator;
+
+namespace CalculatorTestUnit
+{
+    [TestFixture]
+    public class CalculatorUnitTests
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [TestCase(2.5,3.5,6)]
+        [TestCase(-1.5,1.5,0)]
+        [TestCase(-1,-1,-2)]
+        public void TestAdd(double a, double b, double result)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.That(uut.Add(a,b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2.5, 3.5, -1)]
+        [TestCase(-1.5, 1.5, -3)]
+        [TestCase(-1, -1, 0)]
+        public void TestSubtract(double a, double b, double result)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 5, 10)]
+        [TestCase(-1, 5, -5)]
+        [TestCase(-1, -1, 1)]
+        public void TestMultiply(double a, double b, double result)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.That(uut.Multiply(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 5, 32)]
+        [TestCase(7, 4, 2401)]
+        [TestCase(3, 3, 27)]
+        public void TestPower(double x, double exp, double result)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            Assert.That(uut.Power(x, exp), Is.EqualTo(result));
+        }
+
+    }
+}
---
+++ swt01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Calculator!");
+
+            var uut = new Calculator();
+
+            Console.WriteLine("\nTester Add-funktion");
+            Console.WriteLine("Add({0}, {1}) = {2}", 3.5, 2.5, uut.Add(3.5, 2.5));
+
+            Console.WriteLine("\nTester Subtract-funktion");
+            Console.WriteLine("Subtract({0}, {1}) = {2}", 3.5, 2.5, uut.Subtract(3.5, 2.5));
+
+            Console.WriteLine("\nTester Multiply-funktion");
+            Console.WriteLine("Multiply({0}, {1}) = {2}", 3, 2, uut.Multiply(3, 2));
+
+            Console.WriteLine("\nTester Power-funktion");
+            Console.WriteLine("Add({0}, {1}) = {2}", 5, 2, uut.Power(5, 2));
+        }
+    }
+}
